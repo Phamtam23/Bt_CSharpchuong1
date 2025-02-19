df1fe6126993abea5620c64a39e960adb3001b02
Phamtam23 <phamthientam20041@gmail.com>
2025-02-19 23:02:18 +0700

    thêm biến tháng

--- Program.cs
+++ Program.cs
@@ -10,7 +10,12 @@ namespace Chuong1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nTổng là:");
+            //Bài 85: Nhập vào tháng của 1 năm. Cho biết tháng thuộc quý mấy trong năm
+            Console.WriteLine("\nBài 85: Nhập vào tháng của 1 năm. Cho biết tháng thuộc quý mấy trong năm");
+            int month_336;
+
+            Console.Write("Nhập vào tháng (1 - 12): ");
+            month_336 = Convert.ToInt32(Console.ReadLine());
             Console.ReadLine();
         }
     }
