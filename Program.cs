595e4ab50fa3429925e3a5d1bc2746faadbbefd1
Phamtam23 <phamthientam20041@gmail.com>
2025-02-19 16:42:49 +0700

    thêm biên đếm b5

--- Program.cs
+++ Program.cs
@@ -10,6 +10,16 @@ namespace Chuong1
     {
         static void Main(string[] args)
         {
+            /*Bài 43: Hãy đếm số lượng chữ số của số nguyên dương n*/
+            int n_336;
+            int count_336 = 0;
+
+            do
+            {
+                Console.Write("Nhập số nguyên dương n: ");
+            } while (!int.TryParse(Console.ReadLine(), out n_336) || n_336 <= 0);
+
+          
             Console.WriteLine("\nTổng là:");
             Console.ReadLine();
         }
