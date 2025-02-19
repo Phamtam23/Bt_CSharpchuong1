2b6f9a63bd2187ddff603b9226e7d09b3a16c504
Phamtam23 <phamthientam20041@gmail.com>
2025-02-19 12:01:01 +0700

    code bai 1

--- Program.cs
+++ Program.cs
@@ -10,7 +10,25 @@ namespace Chuong1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nTổng là:");
+            int n_336;
+            double S_336 = 0;
+
+            do
+            {
+                Console.Write("\nNhập n: ");
+                if (!int.TryParse(Console.ReadLine(), out n_336) || n_336 < 1)
+                {
+                    Console.WriteLine("\nN phải lớn hơn hoặc bằng 1. Xin nhập lại!");
+                }
+            } while (n_336 < 1);
+
+            for (int i_336 = 1; i_336 <= n_336; i_336++)
+            {
+                S_336 += 1.0 / (2 * i_336);
+            }
+
+            Console.WriteLine("\nTổng là: {0:F6}", S_336);
+        
             Console.ReadLine();
         }
     }
