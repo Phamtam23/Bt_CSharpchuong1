99c45ddb644ed471d49438aceffc0fcc0bec4aa6
Phamtam23 <phamthientam20041@gmail.com>
2025-02-19 17:11:12 +0700

    hoàn thành b6

--- Program.cs
+++ Program.cs
@@ -20,8 +20,18 @@ namespace Chuong1
                 Console.Write("Nhập lại n: ");
             }
             int temp_336 = n_336;
-         
-           
+            while (temp_336 > 0)
+            {
+                int digit_336 = temp_336 % 10; // Lấy chữ số cuối cùng
+                if (digit_336 < min_336)
+                {
+                    min_336 = digit_336; // Cập nhật chữ số nhỏ nhất
+                }
+                temp_336 /= 10; // Bỏ chữ số cuối đã xét
+            }
+
+            Console.WriteLine($"Chữ số nhỏ nhất của {n_336} là: {min_336}");
+
             Console.ReadLine();
         }
     }
