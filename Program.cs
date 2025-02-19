a804d567cd421339f8522d551a7fde5bff7c52ff
Phamtam23 <phamthientam20041@gmail.com>
2025-02-19 17:25:25 +0700

    chia các trường hợp

--- Program.cs
+++ Program.cs
@@ -22,7 +22,40 @@ namespace Chuong1
 
             Console.Write("Nhập c: ");
             c_336 = Convert.ToDouble(Console.ReadLine());
-            
+            if (a_336 == 0)
+            {
+                if (b_336 == 0)
+                {
+                    Console.WriteLine("Phương trình vô nghiệm!");
+                }
+                else
+                {
+                    Console.WriteLine("Phương trình có một nghiệm: x = {0}", -c_336 / b_336);
+                }
+            }
+            else
+            {
+                delta_336 = b_336 * b_336 - 4 * a_336 * c_336;
+
+                if (delta_336 > 0)
+                {
+                    x1_336 = (-b_336 + Math.Sqrt(delta_336)) / (2 * a_336);
+                    x2_336 = (-b_336 - Math.Sqrt(delta_336)) / (2 * a_336);
+                    Console.WriteLine("Phương trình có hai nghiệm phân biệt:");
+                    Console.WriteLine("x1 = {0:F6}", x1_336);
+                    Console.WriteLine("x2 = {0:F6}", x2_336);
+                }
+                else if (delta_336 == 0)
+                {
+                    x1_336 = -b_336 / (2 * a_336);
+                    Console.WriteLine("Phương trình có nghiệm kép: x1 = x2 = {0:F6}", x1_336);
+                }
+                else
+                {
+                    Console.WriteLine("Phương trình vô nghiệm!");
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
