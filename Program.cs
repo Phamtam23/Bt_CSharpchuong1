0083a58844e5f46996475bdf33ba25936f793770
Phamtam23 <phamthientam20041@gmail.com>
2025-02-19 16:48:02 +0700

    hoàn thành b5

--- Program.cs
+++ Program.cs
@@ -13,14 +13,21 @@ namespace Chuong1
             /*Bài 43: Hãy đếm số lượng chữ số của số nguyên dương n*/
             int n_336;
             int count_336 = 0;
-
+            Console.WriteLine("\nBài 43: Hãy đếm số lượng chữ số của số nguyên dương ");
             do
             {
                 Console.Write("Nhập số nguyên dương n: ");
             } while (!int.TryParse(Console.ReadLine(), out n_336) || n_336 <= 0);
 
+            int temp_336 = n_336;
+            while (temp_336 > 0)
+            {
+                count_336++;
+                temp_336 /= 10; 
+            }
+
+            Console.WriteLine("\nSố lượng chữ số của {0} là: {1}", n_336, count_336);
           
-            Console.WriteLine("\nTổng là:");
             Console.ReadLine();
         }
     }
