e0357d09f7cce23c52bf36281dc85d2e66b2ac8a
Phamtam23 <phamthientam20041@gmail.com>
2025-02-19 23:03:43 +0700

    hoàn thành b9

--- Program.cs
+++ Program.cs
@@ -16,6 +16,16 @@ namespace Chuong1
 
             Console.Write("Nhập vào tháng (1 - 12): ");
             month_336 = Convert.ToInt32(Console.ReadLine());
+            if (month_336 >= 1 && month_336 <= 3)
+                Console.WriteLine("Tháng {0} thuộc Quý 1", month_336);
+            else if (month_336 >= 4 && month_336 <= 6)
+                Console.WriteLine("Tháng {0} thuộc Quý 2", month_336);
+            else if (month_336 >= 7 && month_336 <= 9)
+                Console.WriteLine("Tháng {0} thuộc Quý 3", month_336);
+            else if (month_336 >= 10 && month_336 <= 12)
+                Console.WriteLine("Tháng {0} thuộc Quý 4", month_336);
+            else
+                Console.WriteLine("Tháng không hợp lệ! Vui lòng nhập từ 1 đến 12.");
             Console.ReadLine();
         }
     }
