20a5d907f63dd847ce238f81d231e1ad37f5c40a
Phamtam23 <phamthientam20041@gmail.com>
2025-02-19 17:08:01 +0700

    add min và n

--- Program.cs
+++ Program.cs
@@ -10,7 +10,18 @@ namespace Chuong1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\nTổng là:");
+            //  Bài 52: Tìm chữ số nhỏ nhất của số nguyên dương n
+            int n_336, min_336 = 9;
+
+            Console.Write("Nhập số nguyên dương n: ");
+            while (!int.TryParse(Console.ReadLine(), out n_336) || n_336 < 1)
+            {
+                Console.WriteLine("Vui lòng nhập số nguyên dương lớn hơn 0!");
+                Console.Write("Nhập lại n: ");
+            }
+            int temp_336 = n_336;
+         
+           
             Console.ReadLine();
         }
     }
