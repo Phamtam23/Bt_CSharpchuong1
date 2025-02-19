9b2175ddabd0e623eac5d0de6d38278c45a02058
Phamtam23 <phamthientam20041@gmail.com>
2025-02-19 23:10:43 +0700

    viết hàm kt ngày hợp lệ và năm nhuận

--- Program.cs
+++ Program.cs
@@ -8,6 +8,25 @@ namespace Chuong1
 {
     class Program
     {
+        // Hàm kiểm tra ngày hợp lệ
+    static bool IsValidDate(int day_336, int month_336, int year_336)
+        {
+            if (year_336 < 1 || month_336 < 1 || month_336 > 12 || day_336 < 1)
+                return false;
+
+            int[] daysInMonth_336 = { 31, IsLeapYear(year_336) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            if (day_336 > daysInMonth_336[month_336 - 1])
+                return false;
+
+            return true;
+        }
+
+        // Hàm kiểm tra năm nhuận
+        static bool IsLeapYear(int year_336)
+        {
+            return (year_336 % 4 == 0 && year_336 % 100 != 0) || (year_336 % 400 == 0);
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("\nTổng là:");
